bff8f4ef115b4b09f7264f6807c2dac38d2a760f
liyou <liyou@xunlei.com>
2020-11-12 22:05:00 +0800

    geekTime-highFrequency-top30 字符串转换整数 (atoi)

---
+++ src/geektime/high/frequency/top30/04-ContainerWithMostWater.kt
@@ -0,0 +1,25 @@
+package geektime.high.frequency.top30
+
+/**
+ * 8. 字符串转换整数 (atoi)
+ *
+ * https://leetcode-cn.com/problems/string-to-integer-atoi/
+ */
+fun main() {
+    println(maxArea(intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7)))
+    println(maxArea(intArrayOf(1, 1)))
+    println(maxArea(intArrayOf(4, 3, 2, 1, 4)))
+    println(maxArea(intArrayOf(1, 2, 1)))
+}
+
+fun maxArea(height: IntArray): Int {
+    var maxArea = 0
+    var left = 0
+    var right = height.size - 1
+    while (left < right) {
+        val currentArea = height[left].coerceAtMost(height[right]) * (right - left)
+        maxArea = maxArea.coerceAtLeast(currentArea)
+        if (height[left] < height[right]) left++ else right--
+    }
+    return maxArea
+}
