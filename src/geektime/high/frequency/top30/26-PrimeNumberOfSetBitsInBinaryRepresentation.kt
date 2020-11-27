aabb0ed7daf04ef949daab2aa79f8f1544308eff
liyou <liyou@xunlei.com>
2020-11-27 16:09:54 +0800

    geekTime-highFrequency-top30 二进制表示中质数个计算置位

--- src/geektime/high/frequency/top30/26-PrimeNumberOfSetBitsInBinaryRepresentation.kt
+++ src/geektime/high/frequency/top30/26-PrimeNumberOfSetBitsInBinaryRepresentation.kt
@@ -6,4 +6,18 @@ package geektime.high.frequency.top30
  * https://leetcode-cn.com/problems/prime-number-of-set-bits-in-binary-representation/
  */
 fun main() {
+    println(countPrimeSetBits(10, 15))
+}
+
+fun countPrimeSetBits(L: Int, R: Int): Int {
+    var res = 0
+    for (i in L..R) {
+        if (isSmallPrime(Integer.bitCount(i))) res++
+    }
+    return res
+}
+
+
+fun isSmallPrime(x: Int): Boolean {
+    return x == 2 || x == 3 || x == 5 || x == 7 || x == 11 || x == 13 || x == 17 || x == 19
 }
