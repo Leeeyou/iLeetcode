ffebb463a22a456a29ac8c0439893e109f4f6ba5
liyou <liyou@xunlei.com>
2020-11-28 17:36:31 +0800

    geekTime-highFrequency-top30 两句话中的不常见单词

--- src/geektime/high/frequency/top30/29-UncommonWordsFromTwoSentences.kt
+++ src/geektime/high/frequency/top30/29-UncommonWordsFromTwoSentences.kt
@@ -6,4 +6,23 @@ package geektime.high.frequency.top30
  * https://leetcode-cn.com/problems/uncommon-words-from-two-sentences/
  */
 fun main() {
+    uncommonFromSentences("this apple is sweet", "this apple is sour").forEach {
+        println(it)
+    }
+}
+
+fun uncommonFromSentences(A: String, B: String): Array<String> {
+    val count = hashMapOf<String, Int>()
+    A.split(" ").forEach {
+        count[it] = count.getOrDefault(it, 0).plus(1)
+    }
+    B.split(" ").forEach {
+        count[it] = count.getOrDefault(it, 0).plus(1)
+    }
+    val mutableListOf = mutableListOf<String>()
+    count.forEach {
+        if (it.value == 1)
+            mutableListOf.add(it.key)
+    }
+    return mutableListOf.toTypedArray()
 }
