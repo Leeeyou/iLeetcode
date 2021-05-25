072401bcaf52cc4231568bf7e4e800f428849cbb
liyou <liyou@xunlei.com>
2021-05-25 15:54:52 +0800

    medium architecture LRU 缓存机制

--- src/medium/architectrue/LruCache.kt
+++ src/medium/architectrue/LruCache.kt
@@ -1,6 +1,7 @@
 package medium.architectrue
 
 import java.util.*
+import kotlin.collections.HashMap
 
 /**
  * 146. LRU 缓存机制
@@ -37,4 +38,67 @@ class LRUCache(private val capacity: Int) {
             queue.offerFirst(key)
         }
     }
+
+    //// 以下是自定义双向链表
+
+    class DLinkedNode(var key: Int? = null, var value: Int? = null) {
+        var prev: DLinkedNode? = null
+        var next: DLinkedNode? = null
+    }
+
+    var cache = HashMap<Int, DLinkedNode>(capacity)
+
+    // 使用伪头部和伪尾部节点
+    var head = DLinkedNode()
+    var tail = DLinkedNode()
+
+    init {
+        head.next = tail
+        tail.prev = head
+    }
+
+    fun removeNode(node: DLinkedNode) {
+        node.prev?.next = node.next
+        node.next?.prev = node.prev
+    }
+
+    fun addToHead(node: DLinkedNode) {
+        node.prev = head
+        node.next = head.next
+        node.next?.prev = node
+        head.next = node
+    }
+
+    fun moveToHead(node: DLinkedNode) {
+        removeNode(node)
+        addToHead(node)
+    }
+
+    fun removeTail(): DLinkedNode {
+        val node = tail.prev!!
+        removeNode(node)
+        return node
+    }
+
+    fun get2(key: Int): Int {
+        val node = cache[key] ?: return -1
+        moveToHead(node)
+        return node.value!!
+    }
+
+    fun put2(key: Int, value: Int) {
+        if (cache.containsKey(key)) {
+            cache[key]!!.value = value
+            moveToHead(cache[key]!!)
+        } else {
+            val newNode = DLinkedNode(key, value)
+            if (cache.size >= capacity) {
+                val tail = removeTail()
+                cache.remove(tail.key)
+            }
+            cache[key] = newNode
+            addToHead(newNode)
+        }
+    }
+
 }
