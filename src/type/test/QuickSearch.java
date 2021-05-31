926a1da6fc0d1593ab7ccdc0d2c5ff495c3d80f2
liuzhifang <liuzhifang@zhixueyun.com>
2021-06-01 00:04:20 +0800

    增加注释

--- src/type/test/QuickSearch.java
+++ src/type/test/QuickSearch.java
@@ -1,16 +1,41 @@
 package type.test;
 
-import static type.sort.easy._922_SortArrayByParity_IIKt.swap;
-
 public class QuickSearch {
     public static void main(String[] args) {
-        int[] intArray = {0, 0, 1, 2, 4, 2, 2, 3, 1, 4};
-        int[] intArray2 = {3, 44, 38, 5, 47, 15, 2};
-        QuickSearch quickSearch = new QuickSearch();
-        quickSearch.sort(intArray, 0, intArray.length - 1);
-        quickSearch.sort(intArray2, 0, intArray2.length - 1);
-        print(intArray);
-        print(intArray2);
+        int[] array1 = {0, 0, 1, 2, 4, 2, 2, 3, 1, 4};
+        int[] array2 = {3, 44, 38, 5, 47, 15, 2};
+        QuickSearch qs = new QuickSearch();
+        qs.sort(array1, 0, array1.length - 1);
+        qs.sort(array2, 0, array2.length - 1);
+        print(array1);
+        print(array2);
+    }
+
+    public void sort(int[] nums, int low, int hight) {
+        if (low >= hight) return;
+        int mid = partition(nums, low, hight); // 分区函数，返回分区点的下标
+        sort(nums, low, mid - 1); // 递归排序左分区
+        sort(nums, mid + 1, hight); // 递归排序右分区
+    }
+
+    private int partition(int[] nums, int low, int hight) {
+        int pivot = nums[low]; // 以下标low的值为基准点
+        int i = low, j = hight + 1; // j之所以加1是因为下面开始找值的时候，开始就做了--操作
+        while (true) {
+            while (++i <= hight && nums[i] < pivot) ; // 从左到右找第一个大于等于pivot的值
+            while (--j >= low && nums[j] > pivot) ; // 从右到左找第一个小于等于pivot的值
+            if (i >= j) break;
+            startSwap(nums, i, j);
+        }
+        nums[low] = nums[j]; // 将mid的值(也就是下标j的值)与pivot交换
+        nums[j] = pivot;
+        return j;
+    }
+
+    private void startSwap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     private static void print(int[] intArray) {
@@ -19,27 +44,4 @@ public class QuickSearch {
         }
         System.out.println();
     }
-
-    public void sort(int[] nums, int low, int hight) {
-        if (low >= hight) return;
-        int mid = partition(nums, low, hight);
-        sort(nums, low, mid - 1);
-        sort(nums, mid + 1, hight);
-    }
-
-    private int partition(int[] nums, int low, int hight) {
-        int pivot = nums[low];
-        int i = low, j = hight + 1;
-        while (true) {
-            while (++i <= hight && nums[i] < pivot) ;
-            while (--j >= low && nums[j] > pivot) ;
-            if (i >= j) break;
-            swap(nums, i, j);
-        }
-        nums[low] = nums[j];
-        nums[j] = pivot;
-        return j;
-    }
-
-
 }
